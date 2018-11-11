fbe5b982dcadd980ab918340efdd12714819607f
Célian Garcia <celian.garcia1@gmail.com>
2018-11-11 18:45:18 +0100

    Add a visu free field to the ability response

--- pkg/ability/response.go
+++ pkg/ability/response.go
@@ -1,7 +1,10 @@
 package ability
 
-import "milobella/oratio/pkg/anima"
+import (
+	"milobella/oratio/pkg/anima"
+)
 
 type Response struct {
 	Nlg 	anima.NLG	`json:"nlg,omitempty"`
+	Visu	interface{}	`json:"visu,omitempty"`
 }
